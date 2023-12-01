2a420993d02c826edf652a36c915442040cecad2
bitxuji <bitxuji@gmail.com>
2023-12-01 18:38:09 +0900

    ng g c todo-list todo-dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashboardComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
