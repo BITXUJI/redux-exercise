c4adac3e27353719ae0164ad046c1a3554c2f4f8
bitxuji <bitxuji@gmail.com>
2023-12-02 23:04:18 +0900

    delete app/reducer.ts :ActionReducerMap

--- src/app/tasking/tasking.module.ts
+++ src/app/tasking/tasking.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule } from "@angular/common/http";
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
@@ -18,7 +17,6 @@ import { todoReducer } from './reducer';
         StoreModule.forFeature('tdl', todoReducer),
         StoreModule.forFeature('tdd', todoReducer),
         HttpClientModule,
-
     ],
     exports: [
         TodoDashboardComponent,
