02c778a11b3b16e6eb7b31448a496e4d27030bd7
bitxuji <bitxuji@gmail.com>
2023-12-02 22:53:19 +0900

    refatoring the code

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,19 +1,19 @@
-import { ActionReducerMap } from '@ngrx/store';
-import { counterInitialState, counterReducer, counterState } from './countering/reducer';
-import { taskInitialState, taskState, todoReducer } from './tasking/reducer';
+// import { ActionReducerMap } from '@ngrx/store';
+// import { counterInitialState, counterReducer, counterState } from './countering/reducer';
+// import { taskInitialState, taskState, todoReducer } from './tasking/reducer';
 
-// State Interface
-export interface IAppState {
-    tasking: taskState,
-    countering: counterState
-}
+// // State Interface
+// export interface IAppState {
+//     tasking: taskState,
+//     countering: counterState
+// }
 
-// Initial State
-export const initialState: IAppState = {
-    tasking: taskInitialState,
-    countering: counterInitialState
-};
-export const reducers: ActionReducerMap<IAppState> = {
-    tasking: todoReducer,
-    countering: counterReducer
-};
+// // Initial State
+// export const initialState: IAppState = {
+//     tasking: taskInitialState,
+//     countering: counterInitialState
+// };
+// export const reducers: ActionReducerMap<IAppState> = {
+//     tasking: todoReducer,
+//     countering: counterReducer
+// };
--- src/app/tasking/todo.service.ts
+++ src/app/tasking/todo.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { IAppState } from "../reducer";
+import { taskState } from "./reducer";
 import { fetchTodosSuccess } from "./actions";
 @Injectable()
 export class TodoService {
   private readonly url = 'http://jsonplaceholder.typicode.com/posts';
   constructor(private http: HttpClient,
-    private store: Store<{ tdl: IAppState }>) { }
+    private store: Store<{ tdl: taskState }>) { }
   loadTodos() {
     return this.http.get<any[]>(this.url).subscribe(
       serviceTodos => {
