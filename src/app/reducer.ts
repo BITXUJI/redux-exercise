496f81ab1d951674434c782897fec3c3e54bcb93
bitxuji <bitxuji@gmail.com>
2023-12-02 12:52:49 +0900

    rewrite reducer.ts without function createReducer

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,4 +1,56 @@
-import { createReducer, on } from '@ngrx/store';
+// import { createReducer, on } from '@ngrx/store';
+// import { addTodo, clearTodos, removeTodo, toggleTodo } from './actions';
+
+// // State Interface
+// export interface IAppState {
+//     todos: any[];
+//     lastUpdate: Date | null;
+// }
+
+// // Initial State
+// export const initialState: IAppState = {
+//     todos: [],
+//     lastUpdate: null
+// };
+
+// // Reducer
+// const _todoReducer = createReducer(
+//     initialState,
+//     on(addTodo, (state, { title }) => ({
+//         ...state,
+//         todos: [...state.todos, { id: state.todos.length + 1, title }],
+//         lastUpdate: new Date(),
+//     })),
+//     on(toggleTodo, (state, { id }) => {
+//         const todoIndex = state.todos.findIndex(t => t.id === id);
+//         const todo = state.todos[todoIndex];
+//         const updatedTodo = { ...todo, isCompleted: !todo.isCompleted };
+
+//         return {
+//             ...state,
+//             todos: [
+//                 ...state.todos.slice(0, todoIndex),
+//                 updatedTodo,
+//                 ...state.todos.slice(todoIndex + 1),
+//             ],
+//             lastUpdate: new Date(),
+//         };
+//     }),
+//     on(removeTodo, (state, { id }) => ({
+//         ...state,
+//         todos: state.todos.filter(t => !(t.id === id)),
+//         lastUpdate: new Date(),
+//     })),
+//     on(clearTodos, state => ({
+//         ...state,
+//         todos: [],
+//         lastUpdate: new Date(),
+//     })),
+// );
+
+// export function todoReducer(state: any, action: any) {
+//     return _todoReducer(state, action);
+// }
 import { addTodo, clearTodos, removeTodo, toggleTodo } from './actions';
 
 // State Interface
@@ -14,40 +66,45 @@ export const initialState: IAppState = {
 };
 
 // Reducer
-const _todoReducer = createReducer(
-    initialState,
-    on(addTodo, (state, { title }) => ({
-        ...state,
-        todos: [...state.todos, { id: state.todos.length + 1, title }],
-        lastUpdate: new Date(),
-    })),
-    on(toggleTodo, (state, { id }) => {
-        const todoIndex = state.todos.findIndex(t => t.id === id);
-        const todo = state.todos[todoIndex];
-        const updatedTodo = { ...todo, isCompleted: !todo.isCompleted };
+export function todoReducer(state: IAppState = initialState, action: any): IAppState {
+    switch (action.type) {
+        case addTodo.type:
+            return {
+                ...state,
+                todos: [...state.todos, { id: state.todos.length + 1, title: action.title }],
+                lastUpdate: new Date(),
+            };
 
-        return {
-            ...state,
-            todos: [
-                ...state.todos.slice(0, todoIndex),
-                updatedTodo,
-                ...state.todos.slice(todoIndex + 1),
-            ],
-            lastUpdate: new Date(),
-        };
-    }),
-    on(removeTodo, (state, { id }) => ({
-        ...state,
-        todos: state.todos.filter(t => !(t.id === id)),
-        lastUpdate: new Date(),
-    })),
-    on(clearTodos, state => ({
-        ...state,
-        todos: [],
-        lastUpdate: new Date(),
-    })),
-);
+        case toggleTodo.type:
+            const todoIndex = state.todos.findIndex(t => t.id === action.id);
+            const todo = state.todos[todoIndex];
+            const updatedTodo = { ...todo, isCompleted: !todo.isCompleted };
 
-export function todoReducer(state: any, action: any) {
-    return _todoReducer(state, action);
+            return {
+                ...state,
+                todos: [
+                    ...state.todos.slice(0, todoIndex),
+                    updatedTodo,
+                    ...state.todos.slice(todoIndex + 1),
+                ],
+                lastUpdate: new Date(),
+            };
+
+        case removeTodo.type:
+            return {
+                ...state,
+                todos: state.todos.filter(t => t.id !== action.id),
+                lastUpdate: new Date(),
+            };
+
+        case clearTodos.type:
+            return {
+                ...state,
+                todos: [],
+                lastUpdate: new Date(),
+            };
+
+        default:
+            return state;
+    }
 }
